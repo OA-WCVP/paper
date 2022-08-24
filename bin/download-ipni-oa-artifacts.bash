a46fea293d3ff16aad09ee5a401af89c86e91d41
Nicky Nicolson <n.nicolson@kew.org>
2022-08-24 11:54:13 +0100

    Authentication for api access

--- bin/download-ipni-oa-artifacts.bash
+++ bin/download-ipni-oa-artifacts.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
-USER=OA-WCVP \
-REPO=ipni-oa \
+USER=OA-WCVP
+REPO=ipni-oa
 GITHUB_API=https://api.github.com/repos/${USER}/${REPO}/releases/latest
-
+token=${{ secrets.PAT }}
 ARTIFACT=data
-echo "Querying GitHub for the latest $ARCH release" 
-LATEST=$(curl -L -s -H 'Accept: application/json' $GITHUB_API) 
+
+echo "Querying GitHub for the latest $ARTIFACT artifact attached to $REPO release using $GITHUB_API" 
+
+#LATEST=$(curl -L -s -H 'Accept: application/json' $GITHUB_API) 
+LATEST=$(curl -L -s -H 'Accept: application/json' ${GITHUB_API}?access_token=${token}) 
 LATEST_TAG=$(echo $LATEST_CURL | jq -r '.tag_name') 
 echo "Found version $LATEST_TAG" \
 LATEST_URL=$(echo $LATEST | jq -r ".assets[] | select(.name | contains(\"$ARTIFACT\")) | .url") 
 echo "Downloading curl $LATEST_TAG from $USER" 
-curl -vLJO -H 'Accept: application/octet-stream' $LATEST_URL
+curl -vLJO -H 'Accept: application/octet-stream' $LATEST_URL?access_token=${token}
 ls -ltra
